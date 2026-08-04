be7142fbb70d39e04ccd328b9c9da953f13a5cd4
SharadaBende <sharadabende2@gmail.com>
2026-08-05 01:29:23 +0530

    Add Groq API key to settings

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    ANTHROPIC_API_KEY: str = os.getenv("ANTHROPIC_API_KEY", "")
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
+
+settings = Settings()
